edcb5b8359c56e677999b9ac9e76c5f016f50a64
Kirill <k1rill-sm@ya.ru>
2016-10-01 22:22:42 +0300

    Delete unused code

--- client-py/api.py
+++ client-py/api.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 import sys
 import json
 
@@ -9,13 +8,6 @@ from server import config
 
 class API:
   def __init__(self):
-  #   self.html = self.get_or_update_html()
-  #
-  # def get_or_update_html(self):
-  #   req = requests.get(config.URL)
-  #   soup = BeautifulSoup(req.text)
-  #
-  #   self.html = soup.body
     self.data = {}
 
   def get_data(self):
--- server/server.py
+++ server/server.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return jsonify(VICTIM)
-    # return render_template("index.html", victim=VICTIM)
 
 if __name__ == "__main__":
     app.run(port=SERVER["port"])
