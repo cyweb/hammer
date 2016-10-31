2708e9b0acb8253f8ae75a122a82f164531c540e
Kirill Dmirnov <k1ril-sm@ya.ru>
2016-10-31 12:29:38 +0300

    Refactoring server

--- server/server.py
+++ server/server.py
@@ -1,13 +1,6 @@
 from flask import Flask, jsonify
 
-from config import SERVER
-from victim import VICTIM
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return jsonify(VICTIM)
+from app import application
 
 if __name__ == "__main__":
-    app.run(port=SERVER["port"])
+    application.run(port=3000)
